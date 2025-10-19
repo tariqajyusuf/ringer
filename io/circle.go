596d4adb084b023ddeea54c5c8e6124763a48687
Tariq Yusuf <tariq@intentionallyhuman.xyz>
2025-10-19 15:04:31 -0700

    Make Circle marshalable

--- io/circle.go
+++ io/circle.go
@@ -5,5 +5,6 @@ Circle files are declarations of a desired system state. A list of packages is
 declared based on their Guise names.
 */
 type Circle struct {
-	packages []string
+	Marshalable
+	packages []string `yaml:"packages,flow"`
 }
